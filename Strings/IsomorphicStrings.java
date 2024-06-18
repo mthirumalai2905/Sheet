cc7b6d6e0cf5386c3dab4a2bc97e6ae51b6973a6
Thirumalai <thiru@thiru.com>
2024-06-18 20:32:12 +0530

    Added Strings

---
+++ Strings/IsomorphicStrings.java
@@ -0,0 +1,33 @@
+package Strings;
+
+import java.util.HashMap;
+
+class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        //Creating a hashmap
+        HashMap<Character, Character> map = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char original = s.charAt(i);
+            char replacement = t.charAt(i);
+
+            if(!map.containsKey(original)){
+                if(!map.containsValue(replacement)){
+                    map.put(original, replacement);
+                } else {
+                    return false;
+                }
+            } else {
+                char mappedCharacter = map.get(original);
+                if(mappedCharacter != replacement){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
