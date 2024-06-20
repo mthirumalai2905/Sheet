b7a620677307c83e83f3b110fb1b8ad496b937e8
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-06-20 15:31:00 +0530

    Update SumOfBeautyOfAllSubstrings.java

--- Strings/SumOfBeautyOfAllSubstrings.java
+++ Strings/SumOfBeautyOfAllSubstrings.java
@@ -1,3 +1,25 @@
 public class SumOfBeautyOfAllSubstrings {
-    
+    public int beautySum(String s) {
+        int n = s.length();
+        int totalBeautySum = 0;
+
+        for(int i = 0; i < n; i++){
+            HashMap<Character, Integer> frequencyMap = new HashMap<>();
+            for(int j = i; j < n; j++){
+                char currChar = s.charAt(j);
+                frequencyMap.put(currChar, frequencyMap.getOrDefault(currChar, 0) + 1);
+
+                int minFreq = Integer.MAX_VALUE;
+                int maxFreq = Integer.MIN_VALUE;
+
+                for(int freq : frequencyMap.values()){
+                    minFreq = Math.min(minFreq, freq);
+                    maxFreq = Math.max(maxFreq, freq);
+                }
+                totalBeautySum += (maxFreq - minFreq);
+            }
+        }
+        return totalBeautySum;
+    }
+
 }
