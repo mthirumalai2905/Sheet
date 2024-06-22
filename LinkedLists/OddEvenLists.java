b287db60263072d76c759796e9c6f5ec33a5c021
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-06-22 11:41:02 +0530

    Create OddEvenLists.java

---
+++ LinkedLists/OddEvenLists.java
@@ -0,0 +1,23 @@
+class OddEvenLists{
+  public ListNode oddEvenList(ListNode head){
+     if(head == null){
+       return head;
+     }
+
+    ListNode odd = head;
+    ListNode even = head.next;
+    ListNode evenHead = even;
+
+    while(even != null && even.next != null){
+      odd.next = odd.next.next;
+      odd = odd.next;
+
+      even.next = even.next.next;
+      even = even.next;
+    }
+
+    odd.next = evenHead;
+
+    return headl
+   }
+}
