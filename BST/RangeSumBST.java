0744c98d5686b73253b859d20632ecbda2a3d9d4
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-06-25 18:44:14 +0530

    Create RangeSumBST.java

---
+++ BST/RangeSumBST.java
@@ -0,0 +1,20 @@
+package BST;
+
+class RangeSumBST {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root == null){
+            return 0;
+        }
+
+        if(root.val >= low && root.val <= high){
+            return root.val + rangeSumBST(root.left,low,high) + rangeSumBST(root.right,low,high);
+        }
+
+        if(root.val < low){
+            return rangeSumBST(root.right,low,high);
+        }
+    
+            return rangeSumBST(root.left,low,high);
+        
+    }
+}
