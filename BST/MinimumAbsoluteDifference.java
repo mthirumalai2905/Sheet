656e6bfb8e6e726aefc647fb98486865d22d9fdc
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-06-25 18:42:31 +0530

    Create MinimumAbsoluteDifference.java

---
+++ BST/MinimumAbsoluteDifference.java
@@ -0,0 +1,24 @@
+class Solution {
+    private int min;
+    private TreeNode prev;
+
+    public void inOrder(TreeNode root) {
+        if (root == null) return;
+
+        inOrder(root.left);
+
+        if (prev != null) {
+            min = Math.min(min, Math.abs(root.val - prev.val));
+        }
+
+        prev = root;
+        inOrder(root.right);
+    }
+
+    public int getMinimumDifference(TreeNode root) {
+        min = Integer.MAX_VALUE;
+        prev = null;
+        inOrder(root);
+        return min;
+    }
+}
