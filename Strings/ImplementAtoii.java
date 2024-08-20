70cccf104c0ca215aecdc55c6d6a28f3115e332f
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-08-20 19:17:28 +0530

    Create ImplementAtoii.java

---
+++ Strings/ImplementAtoii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int myAtoi(String s) {
+        int i = 0, sign = 1, result = 0, n = s.length();
+
+        while(i < n && s.charAt(i) == ' '){
+            i++;
+        }
+
+        if(i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')){
+            sign = s.charAt(i) == '-' ? -1 : 1;
+            i++;
+        }
+
+        while(i < n && Character.isDigit(s.charAt(i))){
+            int digit = s.charAt(i) - '0';
+            if(result > (Integer.MAX_VALUE - digit) / 10){
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            result = result * 10 + digit;
+            i++;
+        }
+        return sign * result;
+    }
+}
