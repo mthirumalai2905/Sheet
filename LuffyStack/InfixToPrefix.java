39285ff3ddd806d544d4832e72724397683a2198
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-09-10 19:28:33 +0530

    Create InfixToPrefix.java

---
+++ LuffyStack/InfixToPrefix.java
@@ -0,0 +1,8 @@
+1:) Reverse The String
+2:) Convert the '(' -> ')' and vice versa
+3:) stack's upper element has not to be strictly greater than the peek element
+4:) Apply Infix to Postfix
+5:) Reverse the Resulted String
+
+- In infix to postfix we make sure that the element above is strictly greater in priority than the peek element
+- In infix to prefix that's not sure it's priority can be equal to the peek for eg('+' & '-' can be pushed in the stack) 
