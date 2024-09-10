76122d0619566176ffa4364cd3d9005c12b0be0e
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-09-10 13:54:05 +0530

    Create QueueUsingStack.java

---
+++ LuffyStack/QueueUsingStack.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+class MyQueue {
+    Stack<Integer> input;
+    Stack<Integer> output;
+
+    public MyQueue() {
+        input = new Stack<>();
+        output = new Stack<>();
+    }
+
+    public void push(int x) {
+        input.push(x);
+    }
+
+    public int pop() {
+        if (output.isEmpty()) {
+            while (!input.isEmpty()) {
+                output.push(input.pop());
+            }
+        }
+        return output.pop();
+    }
+
+    public int peek() {
+        if (output.isEmpty()) {
+            while (!input.isEmpty()) {
+                output.push(input.pop());
+            }
+        }
+        return output.peek();
+    }
+
+    public boolean empty() {
+        return input.isEmpty() && output.isEmpty();
+    }
+}
